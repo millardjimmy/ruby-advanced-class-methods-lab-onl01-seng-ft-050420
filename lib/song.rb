daaf54c09c08805c77c10e617999fe2d624d4266
Jimmy Millard <millardjimmy@gmail.com>
2020-05-16 18:02:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,18 +47,26 @@ class Song
     sorted
   end
   
-  def self.new_from_filename
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
   end
-  
-  def self.create_from_filename
-  end 
-  
+
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
   def self.destroy_all
-  end 
-  
-  
-  
-  
+    self.all.clear
+  end
 end
 
 
