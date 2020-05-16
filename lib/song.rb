5318087a48660268c9f9b2e5b6260954c532a194
Jimmy Millard <millardjimmy@gmail.com>
2020-05-16 18:02:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,43 +10,44 @@ class Song
     self.class.all << self
   end
 
-  def self.create
-    self.class.all << self
-    self.all << song 
-    song 
+  def self.create #class constructor
+    song = self.new
+    self.all << song
+    song
   end
 
-  def self.new_by_name(title)
-    song = self.new 
-    song.name = song 
-    song 
-  end 
+  def self.new_by_name(title) #class constructor
+    song = self.new
+    song.name = title
+    song
+  end
 
-  def self.create_by_name(title)
-    song = self.create 
-    song.name = title 
-    song 
-  end 
+  def self.create_by_name(title) #class constructor
+    song = self.create
+    song.name = title
+    song
+  end
 
-  def self.find_by_name(title)
+  def self.find_by_name(title) #class finder
     result = self.all.detect {|song| song.name == title}
     result
   end
-  
-  def find_or_create_by_name(title)
+
+  def self.find_or_create_by_name(title)
+    #either return a matching song instance with that name or create a new song with the name and return the song instance
     result = self.find_by_name(title)
     if result
-      puts result
-    else 
-      self.create_by_name
+      result
+    else
+      self.create_by_name(title)
     end
   end
-  
+
   def self.alphabetical
     sorted = self.all.sort_by {|song| song.name}
     sorted
   end
-  
+
   def self.new_from_filename(filename)
     song_array = filename.split(" - ")
     song_array[1] = song_array[1].chomp(".mp3")
@@ -68,23 +69,3 @@ class Song
     self.all.clear
   end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
